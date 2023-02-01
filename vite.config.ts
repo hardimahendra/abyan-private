9a7dd443574a6cb89ff5a7e2eaa1642903aa68b5
Hardi Mahendra <41864461+hardimahendra@users.noreply.github.com>
2023-02-01 16:03:04 +0700

    update vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'index.html'),
-      name: 'MyLib',
-      fileName: 'my-lib',
-    },
-    rollupOptions: {
-      external: ['react'],
-      output: {
-        globals: {
-          vue: 'react',
-        },
-      },
-    },
-  },
+  plugins: [react()]
 })
