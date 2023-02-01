564f86b156f774ab277c44af9a086f3ce84320dc
Hardi Mahendra <41864461+hardimahendra@users.noreply.github.com>
2023-02-01 20:50:53 +0700

    update fix harusnya

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,30 +9,28 @@ export function Navbar(){
                 <nav className="container-navbar">
                 <div className="navbar-wrapper">
                     <div className="brand">
-                        <a>
-                            <Link to="/">
-                                <img src="./assets/icons/abyanprivate.png" alt="brand" />
-                            </Link>
-                        </a>
+                        <Link to="/">
+                            <img src="./assets/icons/abyanprivate.png" alt="brand" />
+                        </Link>
                     </div>
                     <div className="navbar-left">
-                        <a aria-current="page"><Link to="/home">Home</Link></a>
-                        <a><Link to="/profil">Profil</Link> </a>
-                        <a><Link to="/galery">Galeri</Link> </a>
-                        <a><Link to="/daftarGuru">Daftar Guru</Link></a>
-                        <a><Link to="/contacts">Kontak</Link></a>
+                        <Link aria-current="page" to="/home">Home</Link>
+                        <Link to="/profil">Profil</Link>
+                        <Link to="/galery">Galeri</Link>
+                        <Link to="/daftarGuru">Daftar Guru</Link>
+                        <Link to="/contacts">Kontak</Link>
                     </div>
                     <div className="navbar-right">
-                        <a type="button"><Link to="/login">Login</Link></a>
-                        <a type="button"><Link to="/regisMember">Sign Up</Link></a>
+                        <Link to="/login">Login</Link>
+                        <Link to="/regisMember">Sign Up</Link>
                     </div>
                     <div className="navbar-toggle">
                         <nav id="content-toggle">
-                            <a aria-current="page"><Link to="/home">Home</Link></a>
-                            <a><Link to="/profil">Profil</Link></a>
-                            <a><Link to="/galery">Galeri</Link></a>
-                            <a><Link to="/daftarGuru">Daftar Guru </Link></a>
-                            <a><Link to="/contacts">Kontak</Link></a>
+                            <Link aria-current="page" to="/home">Home</Link>
+                            <Link to="/profil">Profil</Link>
+                            <Link to="/galery">Galeri</Link>
+                            <Link to="/daftarGuru">Daftar Guru </Link>
+                            <Link to="/contacts">Kontak</Link>
                             <Dropdown className="dropdown">
                                 <Dropdown.Toggle id="dropdown-split-basic"></Dropdown.Toggle>
                                 <Dropdown.Menu className="dropdown-menu">
